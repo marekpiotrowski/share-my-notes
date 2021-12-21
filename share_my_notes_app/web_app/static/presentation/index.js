1e2076ccb5767a407a7e0f6a8d850f29354c7cca
marekpiotrowski <marek_piotrowski@live.com>
2021-12-21 11:28:34 +0100

    UI basically ready

--- share_my_notes_app/web_app/static/presentation/index.js
+++ share_my_notes_app/web_app/static/presentation/index.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use strict';
 
     angular
@@ -9,66 +9,76 @@
         .module('shareMyNotesApp')
         .controller('ModalInstanceCtrl', ModalInstanceCtrl);
 
-    function SessionController($scope, $uibModal) {
-        var vm = this;
+    function SessionController($scope, $uibModal, $http) {
+        $scope.openedSessions = [];
 
-        vm.openModal = openModal;
-        vm.closeModal = closeModal;
+        $scope.dt = null;
 
-        initController();
+        $scope.open = function() {
+            var modalInstance = $uibModal.open({
+                animation: false,
+                backdropClass: 'modal-backdrop',
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: 'ctrl',
+                size: 'lg'
+            });
 
-        $scope.open = function (size, parentSelector) {
-          var parentElem = parentSelector ?
-            angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-          var modalInstance = $uibModal.open({
-            animation: false,
-            backdropClass: 'modal-backdrop',
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'myModalContent.html',
-            controller: 'ModalInstanceCtrl',
-            size: 'lg',
-            resolve: {
-              items: function () {
-                return "Abc";//["abc", "cde"];//$ctrl.items;
-              }
-            }
-          });
+            modalInstance.result.then(function(sessionData) {
+                $http({
+                    method: "GET",
+                    url: "/sessions"
+                }).then(function mySuccess(response) {
+                    var alreadyExistingSession = response.data;
+                    if (alreadyExistingSession.filter((s) => {
+                            return s.name == sessionData['sessionName'];
+                        }).length != 0) {
+                        // TODO add toast
+                        console.log("session already exists");
+                        return;
+                    }
+                    // TODO add error handler
+                    $http({
+                        method: "POST",
+                        url: "/session",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        data: {
+                            name: sessionData['sessionName'],
+                            password: sessionData['sessionPassword']
+                        }
+                    }).then((response) => {
+                        console.log("session created.", response.data);
+                        $scope.openedSessions.push(response.data);
+                        console.log($scope.openedSessions);
+                    });
+                }, function myError(response) {
+                    console.log(response.statusText);
+                });
+            }, function() {
 
-          modalInstance.result.then(function (selectedItem) {
-            // $ctrl.selected = selectedItem;
-          }, function () {
-            // $log.info('Modal dismissed at: ' + new Date());
-          });
+            });
         };
-
-        function initController() {
-            vm.bodyText = 'This text can be updated in modal 1';
-        }
-
-        function openModal(id){
-            console.log("open modal");
-        }
-
-        function closeModal(id){
-            console.log("close modal");
-        }
     }
 
-    function ModalInstanceCtrl($uibModalInstance, items) {
-      var $ctrl = this;
-      $ctrl.items = items;
-      $ctrl.selected = {
-        item: $ctrl.items[0]
-      };
+    function ModalInstanceCtrl($uibModalInstance) {
+        var $ctrl = this;
+        $ctrl.sessionName = "";
+        $ctrl.sessionPassword = "";
 
-      $ctrl.ok = function () {
-        $uibModalInstance.close($ctrl.selected.item);
-      };
+        $ctrl.ok = function() {
+            $uibModalInstance.close({
+                'sessionName': $ctrl.sessionName,
+                'sessionPassword': $ctrl.sessionPassword
+            });
+        };
 
-      $ctrl.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-      };
+        $ctrl.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
     };
 
 })();
